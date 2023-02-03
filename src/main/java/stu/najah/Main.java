945bd3cf1c3862cf8cf8eee5ca0770469c2b4bd1
Izzat Alsharif <izzat5233@gmail.com>
2023-02-03 17:51:07 +0200

    initial test for the project with no functionalities

--- src/main/java/stu/najah/Main.java
+++ src/main/java/stu/najah/Main.java
@@ -23,6 +23,10 @@ public class Main {
         return true;
     }
 
+    int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
