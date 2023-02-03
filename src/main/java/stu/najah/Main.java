4fe1de6a251ec849be8c2c97ad15faa484d90407
Izzat Alsharif <izzat5233@gmail.com>
2023-02-03 17:38:27 +0200

    initial test for the project with no functionalities

---
+++ src/main/java/stu/najah/Main.java
@@ -0,0 +1,29 @@
+package stu.najah;
+
+public class Main {
+
+    /**
+     * checks if x is a prime number or not.
+     * the test is done by dividing x to all numbers from 2 up to sqrt(x),
+     * if at least one number divides x then x is not a prime,
+     * Note that if x is 0, 1, or negative it's not a prime.
+     * @param x to be tested
+     * @return whether x is prime or not
+     */
+    boolean isPrime(int x) {
+        if(x < 2) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(x);
+        for (int i = 2; i <= sqrt; i++) {
+            if(x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/stu/najah/MainTest.java
@@ -0,0 +1,30 @@
+package stu.najah;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    /**
+     * To be tested
+     */
+    Main main = new Main();
+
+    @Test
+    void isPrime() {
+        // not primes
+        assertFalse(main.isPrime(-5));
+        assertFalse(main.isPrime(0));
+        assertFalse(main.isPrime(1));
+        assertFalse(main.isPrime(4));
+        assertFalse(main.isPrime(100));
+        assertFalse(main.isPrime(22222222));
+        // primes
+        assertTrue(main.isPrime(2));
+        assertTrue(main.isPrime(7));
+        assertTrue(main.isPrime(13));
+        assertTrue(main.isPrime(101));
+    }
+}
