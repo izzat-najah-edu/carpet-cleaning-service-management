b885c5a9d7802902b66bce138ea5e65a0837ae9d
Izzat Alsharif <izzat5233@gmail.com>
2023-03-06 14:35:36 +0200

    refactoring

--- src/main/java/stu/najah/se/dao/CustomerDAO.java
+++ src/main/java/stu/najah/se/dao/CustomerDAO.java
@@ -1,77 +1,22 @@
 package stu.najah.se.dao;
 
-import jakarta.persistence.criteria.CriteriaQuery;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import stu.najah.se.Navigator;
 import stu.najah.se.sql.entity.CustomerEntity;
 
-import java.util.List;
-
 public class CustomerDAO
-    implements DAO<CustomerEntity> {
+        implements DAO<CustomerEntity> {
 
-    @Override
-    public boolean update(CustomerEntity data) {
-        var session = Navigator.getSession();
-        var transaction = session.getTransaction();
-        try {
-            transaction.begin();
-            session.merge(data);
-            transaction.commit();
-            return true;
-        } catch (Exception e) {
-            if(transaction != null) {
-                transaction.rollback();
-            }
-            Navigator.prompt(e.getMessage());
-            return false;
-        }
-    }
-
-    @Override
-    public boolean insert(CustomerEntity data) {
-        var session = Navigator.getSession();
-        var transaction = session.getTransaction();
-        try {
-            transaction.begin();
-            session.persist(data);
-            transaction.commit();
-            return true;
-        } catch (Exception e) {
-            if(transaction != null) {
-                transaction.rollback();
-            }
-            Navigator.prompt(e.getMessage());
-            return false;
-        }
-    }
-
-    @Override
-    public boolean delete(CustomerEntity data) {
-        var session = Navigator.getSession();
-        var transaction = session.getTransaction();
-        try {
-            transaction.begin();
-            session.remove(data);
-            transaction.commit();
-            return true;
-        } catch (Exception e) {
-            if(transaction != null) {
-                transaction.rollback();
-            }
-            Navigator.prompt(e.getMessage());
-            return false;
-        }
-    }
-
-    @Override
+    /**
+     * @return all recorded customers
+     */
     public ObservableList<CustomerEntity> getAll() {
         var session = Navigator.getSession();
         var builder = session.getCriteriaBuilder();
-        CriteriaQuery<CustomerEntity> query = builder.createQuery(CustomerEntity.class);
+        var query = builder.createQuery(CustomerEntity.class);
         query.from(CustomerEntity.class);
-        List<CustomerEntity> list = session.createQuery(query).getResultList();
+        var list = session.createQuery(query).getResultList();
         session.close();
         return FXCollections.observableArrayList(list);
     }
--- src/main/java/stu/najah/se/dao/DAO.java
+++ src/main/java/stu/najah/se/dao/DAO.java
@@ -1,15 +1,68 @@
 package stu.najah.se.dao;
 
-import javafx.collections.ObservableList;
+import stu.najah.se.Navigator;
 
 /**
  * Data Access Object: a layer that separates
  * the sql operations (Hibernate) and any other service using it, like JavaFX
+ *
  * @param <T> an entity class (Admin, Customer...)
  */
 public interface DAO<T> {
-    boolean update(T data);
-    boolean insert(T data);
-    boolean delete(T data);
-    ObservableList<T> getAll();
+    default boolean update(T data) {
+        var session = Navigator.getSession();
+        var transaction = session.getTransaction();
+        try {
+            transaction.begin();
+            session.merge(data);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            Navigator.prompt(e.getMessage());
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    default boolean insert(T data) {
+        var session = Navigator.getSession();
+        var transaction = session.getTransaction();
+        try {
+            transaction.begin();
+            session.persist(data);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            Navigator.prompt(e.getMessage());
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    default boolean delete(T data) {
+        var session = Navigator.getSession();
+        var transaction = session.getTransaction();
+        try {
+            transaction.begin();
+            session.remove(data);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            Navigator.prompt(e.getMessage());
+            return false;
+        } finally {
+            session.close();
+        }
+    }
 }
