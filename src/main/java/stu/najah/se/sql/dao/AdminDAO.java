2b3c344b5fa58a5a2885341d6511ab475f84ec93
Izzat Alsharif <izzat5233@gmail.com>
2023-03-06 14:47:34 +0200

    new admin DAO

---
+++ src/main/java/stu/najah/se/sql/dao/AdminDAO.java
@@ -0,0 +1,17 @@
+package stu.najah.se.sql.dao;
+
+import stu.najah.se.Navigator;
+import stu.najah.se.sql.entity.AdminEntity;
+
+public class AdminDAO
+        implements DAO<AdminEntity> {
+
+    /**
+     * @param username of the admin
+     * @return the AdminEntity with the given username
+     * or null if it's not found
+     */
+    public AdminEntity find(String username) {
+        return Navigator.getSession().find(AdminEntity.class, username);
+    }
+}
