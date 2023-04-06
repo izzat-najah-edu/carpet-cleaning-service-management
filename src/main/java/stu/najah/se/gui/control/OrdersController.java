27b5a7b5386123f9087ffebd7bd291b53ce2ffb5
Izzat Alsharif <izzat5233@gmail.com>
2023-04-07 02:14:18 +0300

    orders panel implemented

--- src/main/java/stu/najah/se/gui/control/OrdersController.java
+++ src/main/java/stu/najah/se/gui/control/OrdersController.java
@@ -1,6 +1,15 @@
 package stu.najah.se.gui.control;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import stu.najah.se.data.dao.OrderDAO;
+import stu.najah.se.data.entity.OrderViewEntity;
+import stu.najah.se.data.entity.ProductEntity;
 import stu.najah.se.gui.Controller;
 
 import java.net.URL;
@@ -9,6 +18,32 @@ import java.util.ResourceBundle;
 public class OrdersController
         implements Controller, Initializable {
 
+    @FXML
+    private CheckBox checkBoxFinished;
+
+    @FXML
+    private Label labelOrderStatus;
+
+    @FXML
+    private TableView<OrderViewEntity> tableOrders;
+
+    @FXML
+    private TableView<ProductEntity> tableProducts;
+
+    @FXML
+    private TextField textFieldCustomerName;
+
+    @FXML
+    private TextField textFieldPrice;
+
+    @FXML
+    private TextField textFieldSpecialTreatment;
+
+    private final OrderDAO orderDAO = new OrderDAO();
+
+    private OrderViewEntity selectedOrder = null;
+    private ProductEntity selectedProduct = null;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -16,6 +51,41 @@ public class OrdersController
 
     @Override
     public void reset() {
+
+    }
+
+    @FXML
+    void clearSelectedProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateInvoice(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refreshOrdersTable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refreshProductsTable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateProduct(ActionEvent event) {
 
     }
 }
