bf869c8c30e95036dbfbdd2ef15de2a4972fc155
ayakhammash <ayosh.kh1@gmail.com>
2023-04-18 20:57:54 +0300

    order product service created

---
+++ src/test/java/features/Logout.java
@@ -0,0 +1,34 @@
+public class LogoutFeature {
+    private boolean isLoggedIn = false; // assume user is initially not logged in
+
+
+    public void login(String username, String password) {
+
+        isLoggedIn = true;
+    }
+
+    // method to log out the user
+    public void logout() {
+        if (isLoggedIn) {
+            // perform logout cleanup here
+            isLoggedIn = false;
+            System.out.println("You have successfully logged out.");
+        } else {
+            System.out.println("You are not currently logged in.");
+        }
+    }
+
+    // main method to test the logout feature
+    public static void main(String[] args) {
+        LogoutFeature logoutFeature = new LogoutFeature();
+
+
+        logoutFeature.isLoggedIn = true;
+
+
+        logoutFeature.logout();
+
+
+        logoutFeature.logout();
+    }
+}
---
+++ src/test/java/features/Review.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ReviewFeature {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Rate the software program out of five stars (1-5): ");
+        int rating = scanner.nextInt();
+        scanner.nextLine();
+
+
+        System.out.print("Write a review title: ");
+        String title = scanner.nextLine();
+
+
+        System.out.print("Write a review body: ");
+        String body = scanner.nextLine();
+
+
+        System.out.print("List the pros of the software program: ");
+        String pros = scanner.nextLine();
+        System.out.print("List the cons of the software program: ");
+        String cons = scanner.nextLine();
+
+
+        System.out.print("Would you recommend this software program? (yes or no): ");
+        String recommend = scanner.nextLine();
+
+
+        System.out.println("\nReview Summary:");
+        System.out.println("Rating: " + rating + " stars");
+        System.out.println("Title: " + title);
+        System.out.println("Body: " + body);
+        System.out.println("Pros: " + pros);
+        System.out.println("Cons: " + cons);
+        System.out.println("Recommend: " + recommend);
+    }
+}
