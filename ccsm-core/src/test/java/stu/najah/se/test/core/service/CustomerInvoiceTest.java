75533038453e400db51c99497d17e9057a8de9d2
ayakhammash <ayosh.kh1@gmail.com>
2023-04-18 22:45:15 +0300

    refactoring

--- ccsm-core/src/test/java/stu/najah/se/test/core/service/CustomerInvoiceTest.java
+++ ccsm-core/src/test/java/stu/najah/se/test/core/service/CustomerInvoiceTest.java
@@ -1,19 +1,66 @@
 package stu.najah.se.test.core.service;
 
+import org.junit.jupiter.api.Test;
 import stu.najah.se.core.service.CustomerInvoice;
 
-import java.util.ArrayList;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerInvoiceTest {
 
-    public static void main(String[] args) {
-        ArrayList<String> itemsToClean = new ArrayList<String>();
-        itemsToClean.add("Carpet");
-        itemsToClean.add("Sofa");
-        itemsToClean.add("Drapes");
+    @Test
+    public void testPrintInvoice() {
+        String input = "Sara\n123 Main St\n456 Second St\nshirt\ndress\npants\ndone\n50.00";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
 
-        CustomerInvoice invoice = new CustomerInvoice("Sara Smith", "123 Main Street, Anytown, USA", "456 Elm Street, Anytown, USA", itemsToClean, 150.00);
+        CustomerInvoice.main(new String[0]);
 
-        invoice.printInvoice();
+        String output = systemOut().getHistory();
+
+        String expectedOutput = "=================================\n" +
+                "CUSTOMER INFORMATION\n" +
+                "=================================\n" +
+                "Customer Name: Sara\n" +
+                "Customer Address: 123 Main St\n" +
+                "Delivery Address: 456 Second St\n" +
+                "=================================\n" +
+                "ITEMS TO CLEAN\n" +
+                "=================================\n" +
+                "- shirt\n" +
+                "- dress\n" +
+                "- pants\n" +
+                "=================================\n" +
+                "TOTAL PRICE\n" +
+                "=================================\n" +
+                "$50.0\n" +
+                "=================================\n";
+
+        assertEquals(expectedOutput, output);
+    }
+
+    private static TestHelper.SystemOut systemOut() {
+        return new TestHelper.SystemOut();
+    }
+
+    static class TestHelper {
+        static class SystemOut {
+            private final java.io.ByteArrayOutputStream outContent = new java.io.ByteArrayOutputStream();
+            private final java.io.PrintStream originalOut = System.out;
+
+            void setAsSystemOut() {
+                System.setOut(new java.io.PrintStream(outContent));
+            }
+
+            void restoreOriginal() {
+                System.setOut(originalOut);
+            }
+
+            String getHistory() {
+                return outContent.toString();
+            }
+        }
     }
 }
