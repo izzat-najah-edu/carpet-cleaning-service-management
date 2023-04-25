ab180787963223656adf2da8bc093203871c79ed
ayakhammash <ayosh.kh1@gmail.com>
2023-04-25 23:14:02 +0300

    order product crud test

--- ccsm-core/src/main/java/stu/najah/se/core/service/GenerateStatistics.java
+++ ccsm-core/src/main/java/stu/najah/se/core/service/GenerateStatistics.java
@@ -78,37 +78,6 @@ public class GenerateStatistics {
     }
 }
 
-public class Invoice {
-
-    private int deliveredItems;
-    private double total;
-    private double cash;
-    private boolean paid;
-
-    public Invoice(int deliveredItems, double total, double cash, boolean paid) {
-        this.deliveredItems = deliveredItems;
-        this.total = total;
-        this.cash = cash;
-        this.paid = paid;
-    }
-
-    public int getDeliveredItems() {
-        return deliveredItems;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public double getCash() {
-        return cash;
-    }
-
-    public boolean isPaid() {
-        return paid;
-    }
-}
-
 
 
 
---
+++ ccsm-core/src/main/java/stu/najah/se/core/service/Invoice.java
@@ -0,0 +1,32 @@
+package stu.najah.se.core.service;
+
+public class Invoice {
+
+    private int deliveredItems;
+    private double total;
+    private double cash;
+    private boolean paid;
+
+    public Invoice(int deliveredItems, double total, double cash, boolean paid) {
+        this.deliveredItems = deliveredItems;
+        this.total = total;
+        this.cash = cash;
+        this.paid = paid;
+    }
+
+    public int getDeliveredItems() {
+        return deliveredItems;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public double getCash() {
+        return cash;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+}
