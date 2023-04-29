af365c0bd51cf635417d8de57039d7de566a503d
ayakhammash <ayosh.kh1@gmail.com>
2023-04-29 23:10:17 +0300

    sonar project key fix

---
+++ ccsm-ui/src/test/java/stu/najah/se/test/ui/features/aya/Configuration.java
@@ -0,0 +1,31 @@
+package stu.najah.se.test.ui.features.aya;
+
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
+import stu.najah.se.core.ServiceManager;
+import stu.najah.se.ui.Prompter;
+import stu.najah.se.ui.SceneManager;
+
+public class Configuration {
+
+    @BeforeAll
+    public static void launch() throws Exception {
+        ServiceManager.initializeAdminService();
+        ServiceManager.initializeEntityServices(Prompter.getInstance(), Prompter.getInstance());
+        ApplicationTest.launch(SceneManager.class);
+    }
+
+    @Before
+    public void waitForBackgroundEffects() {
+        WaitForAsyncUtils.waitForFxEvents();
+    }
+
+    @AfterStep
+    public void waitForStepEffects() {
+        WaitForAsyncUtils.waitForFxEvents();
+    }
+
+}
